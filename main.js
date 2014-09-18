720190721de54203dc65d4091e56bc17569a5200
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-09-18 12:28:55 +0100

    About to change strategy

--- main.js
+++ main.js
@@ -1,72 +1,22 @@
-
 var target;
 var camera;
-var mouseButton=false;
-var mouseX; var mouseY;
+
+// Do this instead:
+//http://phrogz.net/tmp/canvas_zoom_to_cursor.html
 
 // Run on startup
 window.onload=main;
 
 function resize() {
-    gc.width  = window.innerWidth*0.98;
-    gc.height = window.innerHeight*0.92;
+    gc.width  = Math.floor(window.innerWidth-20);
+    gc.height = Math.floor(window.innerHeight-80);
     redraw();
-}
-
-function draw_grid() {
-    var gs=40;
-    var ox = camera.x % gs;
-    var oy = camera.y % gs;
-    var nx = Math.ceil(gc.width/gs);
-    var ny = Math.ceil(gc.height/gs);
-
-    gd.strokeStyle= '#cccccc';
-    gd.strokeWidth=.1;
-    gd.beginPath();
-    
-    for (var i=0; i<nx; i++) { 
-        gd.moveTo(i*gs+ox, 0); 
-        gd.lineTo(i*gs+ox, gc.height); 
-    }
-
-    for (var i=0; i<ny; i++) {
-        gd.moveTo(0, i*gs+oy); 
-        gd.lineTo(gc.width, i*gs+oy); 
-    }
-
-    gd.stroke();
 }
 
 function redraw() {
     gd.clearRect(0, 0, gc.width ,gc.height);
     draw_grid();
 }
-
-function fixMouse(evt) {
-    mouseX=evt.layerX-gc.offsetLeft;
-    mouseY=evt.layerY-gc.offsetTop;
-}
-
-function mouseDown(evt) {
-    fixMouse(evt);
-    mouseButton=true;
-}
-
-function mouseMove(evt) {
-    fixMouse(evt);
-    if (mouseButton){
-        console.log(mouseX, mouseY);
-        camera.x=mouseX;
-        camera.y=mouseY;
-        redraw();
-    }
-}
-
-function mouseUp(evt) {
-    fixMouse(evt);
-    mouseButton=false;
-}
-
 
 function main() {
     // Set up the drawing environment
@@ -79,8 +29,10 @@ function main() {
     gc.addEventListener("mousedown", mouseDown);
     gc.addEventListener("mousemove", mouseMove);
     gc.addEventListener("mouseup", mouseUp);
+    gc.addEventListener("mousewheel", mouseScroll, false);
+    gc.addEventListener('DOMMouseScroll', mouseScroll, false);
 
-    camera = {"x":0, "y":0, "z":1};
+    camera = {"x":0, "y":0, "z":.5};
     console.log('ready 2 go');
     resize();
 }
