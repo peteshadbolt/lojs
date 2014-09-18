f5df53daf356e67dc4df1c19ca359e22a1c7be42
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-09-18 14:46:19 +0100

    Fixed it

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 var target;
-var camera;
-
-// Do this instead:
-//http://phrogz.net/tmp/canvas_zoom_to_cursor.html
 
 // Run on startup
 window.onload=main;
@@ -22,24 +18,15 @@ function main() {
     // Set up the drawing environment
     gc=document.getElementById('canvas');
     gd=gc.getContext('2d');
-    
 
     // Bind a bunch of events
     window.onresize=resize;
-    gc.addEventListener("mousedown", mouseDown);
-    gc.addEventListener("mousemove", mouseMove);
-    gc.addEventListener("mouseup", mouseUp);
-    gc.addEventListener("mousewheel", mouseScroll, false);
-    gc.addEventListener('DOMMouseScroll', mouseScroll, false);
+    bindMouse();
 
-    camera = {"x":0, "y":0, "z":.5};
     console.log('ready 2 go');
     resize();
 }
 
 
 // Dumping ground
-// Bind window resize events 
-//$(window).resize(fillScreen);
-// Load the json
 //$.getJSON('geometry', onGeometry); }
--- mouse.js
+++ mouse.js
@@ -5,16 +5,22 @@ var mouse = {"button":false,
     "cx":0, "cy":0, 
     "dcx":0, "dcy":0,}
 
+function bindMouse() {
+    gc.addEventListener("mousedown", mouseDown);
+    gc.addEventListener("mousemove", mouseMove);
+    gc.addEventListener("mouseup", mouseUp);
+    gc.addEventListener("mousewheel", mouseScroll, false);
+    gc.addEventListener('DOMMouseScroll', mouseScroll, false);
+}
+
 function fixMouse(evt) {
     mouse.xo = mouse.x; mouse.yo=mouse.y;
     mouse.x = evt.offsetX || (evt.pageX - canvas.offsetLeft);
     mouse.y = evt.offsetY || (evt.pageY - canvas.offsetLeft);
     mouse.dx=mouse.x-mouse.xo;
     mouse.dy=mouse.y-mouse.yo;
-    var pt1 = gd.transformedPoint(mouse.x, mouse.y);
-    var pt2 = gd.transformedPoint(mouse.xo, mouse.yo);
-    mouse.cx = pt1.x; mouse.cy = pt1.cy;
-    mouse.dcx = pt2.x - pt1.x; mouse.dcy = pt2.y - pt1.y;
+    //mouse.cx = pt1.x; mouse.cy = pt1.cy;
+    //mouse.dcx = pt2.x - pt1.x; mouse.dcy = pt2.y - pt1.y;
 }
 
 function mouseDown(evt) {
@@ -25,8 +31,9 @@ function mouseDown(evt) {
 function mouseMove(evt) {
     fixMouse(evt);
     if (mouse.button){
-        gc.translate(mouse.dcx, mouse.dcy);
         requestAnimationFrame(redraw);
+        camera.x+=mouse.dx;
+        camera.y+=mouse.dy;
     }
 }
 
@@ -37,8 +44,7 @@ function mouseUp(evt) {
 
 function mouseScroll(evt){
     var delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
-    delta = delta > 0 ? .01 : -.01;
-    console.log(delta);
+    delta = delta > 0 ? .05 : -.05;
     camera.z+=delta;
     requestAnimationFrame(redraw);
 }
