d4e33cc1a930200530837035e4e63398ddfe52a9
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-11-27 15:58:55 +0000

    Simulation now works. TODO: phase/ratio control

--- lojs/urls.py
+++ lojs/urls.py
@@ -3,15 +3,11 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 
-from django.contrib import admin
-admin.autodiscover()
-
 import app.views
 
 urlpatterns = patterns('',
     url(r'^$', app.views.index, name='index'),
-    url(r'^db', app.views.db, name='db'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^simulate', app.views.simulate, name='simulate')
 )
 
 urlpatterns += staticfiles_urlpatterns()
