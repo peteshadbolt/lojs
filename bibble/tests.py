edc3e7a4bf51b7a872e521812ff7849988d75988
Pete Shadbolt <pete.shadbolt@gmail.com>
2015-06-26 18:39:14 +0100

    Tests

--- bibble/tests.py
+++ bibble/tests.py
@@ -3,33 +3,39 @@ from bibble import views
 from django.http import HttpRequest
 import json
 
+MZI =  [ { "type" : "fockstate","x" : 0,"y" : 0,"n" : 1},
+         { "type" : "coupler","x" : 1,"y" : 0,"ratio" : 0.5},
+         { "type" : "phaseshifter","x" : 2,"y" : 0,"phase" : 0},
+         { "type" : "coupler","x" : 3,"y" : 0,"ratio" : 0.5}]
 
-class APITestCase(TestCase):
+class IndexTestCase(TestCase):
     def setUp(self):
         pass
-
+ 
     def test_index(self):
         """ See that we can load the index """
         request = {}
         index = views.index(request)
         assert(len(index.content)>0)
 
+
+class APITestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_no_nonzero(self):
+        """ What happens when have an empty circuit ? """
+        postdata = {"circuit" : []}
+        request = {"body": json.dumps(postdata)}
+        output = views.simulate(request)
+        self.assertEqual(output.status_code, 400)
+
     def test_mzi(self):
         """ See that we can simulate an MZI"""
-
-        circuit = [ { "type" : "fockstate","x" : 0,"y" : 0,"n" : 1},
-                    { "type" : "coupler","x" : 1,"y" : 0,"ratio" : 0.5},
-                    { "type" : "phaseshifter","x" : 2,"y" : 0,"phase" : 0},
-                    { "type" : "coupler","x" : 3,"y" : 0,"ratio" : 0.5}]
-
-        postdata = {"circuit" : circuit,
-                    "rules" : "",
-                    "output_mode" : "probability"}
-
-        request = {"body": json.dumps(postdata)}
+        postdata = {"circuit" : MZI, "format":"dict"}
+        request = {"body": json.dumps(postdata), "format":"dict"}
         output = json.loads(views.simulate(request).content)
+        self.assertAlmostEqual(output["(1,)"]["imag"], 1)
         
-        #TODO: This sucks
-        self.assertEqual(output["values"][0][1]["real"], 0.0)
 
 
--- bibble/views.py
+++ bibble/views.py
@@ -1,46 +1,61 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt       
+from django.views.decorators.csrf import csrf_exempt
 import json
 import sys
 import linear_optics as lo
 import numpy as np
 
+
 class SizeError(Exception):
     pass
 
-# Create your views here.
+
 def index(request):
+    """ Just return the main page """
     return render(request, "index.html", {})
 
-def warning(text):
-    return HttpResponse(json.dumps({"warning":text}), content_type="application/json")
+
+def serialize_to_dict(data):
+    """ Serialize data to JSON """
+    skey = lambda x: str(x)
+    sim = lambda x: {"real": x.real, "imag": x.imag}
+    return {skey(key): sim(value) for key, value in data.items()}
+
+def serialize_to_table(data):
+    """ Serialize data to a table """
+    return [(key, value.real, value.imag) for key, value in data.items()]
+
 
 @csrf_exempt
 def simulate(request):
+    """ Simulates the given circuit """
     request = json.loads(request["body"])
-
-    circuit = request["circuit"]
-    if len(circuit)==0: return warning("Empty circuit")
-    rules = request["rules"]
+    circuit = request.get("circuit", [])
+    rules = request.get("rules", "")
+    format = request.get("format", "table")
 
     # Build a python object describing the circuit, state, patterns of interest
     circuit = lo.compile(circuit, rules)
     nterms = len(circuit["patterns"])
 
-    if nterms > 10000: return warning("Too many terms")
-    if nterms == 0: return warning("No terms match filter")
+    # Check for further 400 errors
+    if nterms > 10000:
+        return HttpResponse("Too many terms", status=400)
+    elif nterms == 0:
+        return HttpResponse("No matching terms", status=400)
 
-    data = lo.simulate(circuit["input_state"], circuit["unitary"], circuit["patterns"])
-    data = data.items()
-    data.sort(key=lambda x: -np.abs(x[1])**2)
+    # Do the actual simulation
+    data = lo.simulate(**circuit)
+    if len(data) == 0:
+        return HttpResponse("No nonzero terms", status=400)
 
-    if len(data) == 0: return warning("No nonzero terms")
+    # Serialize
+    if format == "table":
+        data = serialize_to_table(data)
+    elif format == "dict":
+        data = serialize_to_dict(data)
+    response = json.dumps(data)
 
-    maximum = max([np.abs(x[1])**2 for x in data])
-    serialize = lambda (label, value): (label, {"real": value.real, "imag": value.imag})
-    data = map(serialize, data)
-    output={"values": data, "maximum": 1 if maximum==0 else maximum}
-
-    response=json.dumps(output)
     return HttpResponse(response, content_type="application/json")
+
